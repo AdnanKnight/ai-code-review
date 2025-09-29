b55b6326e52ebd73304208940907e4653647492c
AdnanKnight <adnan555legend@gmail.com>
2025-09-29 23:11:00 +0530

    Feat: Landing page 50% Done!

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,8 +1,31 @@
+import GenImg1 from "../assets/GenImg1.png";
 
 const home = () => {
     return (
-        <div>
-            Hello
+        <div className="heroLanding">
+            <div className="heroText">
+                <h1 className="heroTitle">Unleash your <br /> <span>creativity</span> <br /> without the fear <br /> of Errors</h1>
+                <p>Your coding assistant is here 24/7. Feeling anxious or <br /> burned out? It supports your ideas and shapes it.</p>
+                <h3>Try rev'it Now <i className="ri-arrow-right-up-line"></i></h3>
+            </div>
+            <div className="heroImg">
+                <img src={GenImg1} alt="" className="heroGenImg"/>
+                <ul className="heroList1">
+                    <li className="heroList1Tile">Blogs</li>
+                    <li className="heroList1Tile">About</li>
+                    <li className="heroList1Tile">Github</li>
+                    <li className="heroList1Tile">Linkedin</li>
+                    <li className="heroList1Tile">Hire</li>
+                </ul>
+                <ul className="heroList2">
+                    <li className="heroList2Tile glass">
+                        Users
+                    </li>
+                    <li className="heroList2Tile glass">
+                        Time Saved
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
--- client/src/layouts/homeLayout.jsx
+++ client/src/layouts/homeLayout.jsx
@@ -4,17 +4,17 @@ const homeLayout = () => {
     return (
         <div className='homeLayout'>
             <main>
-                <div className="brandlogo icons">
-                    <i class="ri-quill-pen-ai-line"></i>
+                <div className="brandlogo icons glass">
+                    <i className="ri-quill-pen-ai-line"></i>
                 </div>
-                <div className="signinlogo icons">
-                    <i class="ri-account-circle-line"></i>
+                <div className="signinlogo icons glass">
+                    <i className="ri-account-circle-line"></i>
                 </div>
                 <Outlet />
             </main>
 
             <nav>
-                <div className="navbar">
+                <div className="navbar glass">
                     <h3>Home</h3>
                     <h3>Code</h3>
                     <h3>Academy</h3>
